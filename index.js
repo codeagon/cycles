916502f039be5fa3938a9323db562571e9b14a31
Pentagon <pentagonalpentagon@gmail.com>
2018-06-17 20:56:20 +0800

    D

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Command = require('command');
+const path = require('path');
+const fs = require('fs');
+
 const aero = ["VK_Aeroset.VK_SkyCastle00_AERO", "aen_aeroset.AERO.Serpent_Island_AERO_FINAL", //add or change things to this if you want
     "aen_aeroset.AERO.AEN_C_Misty_Island_Outdoor_AERO", "RNW_Aeroset_Various.AERO.Sunset_AERO",
     "atw_aeroset.AERO.SPR_Dawn_Garden_02_AERO", "lobby_ch_select_aero.AERO.Lobby_CH_Select_AERO_Night_02"];
@@ -6,7 +9,16 @@ const aero = ["VK_Aeroset.VK_SkyCastle00_AERO", "aen_aeroset.AERO.Serpent_Island
 module.exports = function Cycles(dispatch) {
     const command = Command(dispatch);
     let count = 0;
-
+// heuheuea
+    try {
+        config = require('./config.json');
+    } catch (e) {
+        config = {
+            onMapChange: false
+        };
+        saveConfig();
+    }
+    
     dispatch.hook('C_LOAD_TOPO_FIN', 1, (event) => {
         if (config.onMapChange) {
             bleb = setInterval(timer, 120000);
@@ -37,14 +49,6 @@ module.exports = function Cycles(dispatch) {
 
 
     //eheheheahEHehaehueh
-    try {
-        config = require('./config.json');
-    } catch (e) {
-        config = {
-            onMapChange = false
-        };
-        saveConfig();
-    }
 
     function saveConfig() {
         fs.writeFile(path.join(__dirname, 'config.json'), JSON.stringify(
